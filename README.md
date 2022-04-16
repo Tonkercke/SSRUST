c0bf891056b9a3fcf44f588a5a2ecb279ccf8c7e
Tonkercke <another2468@protonmail.com>
2022-04-16 13:25:48 +0900

    2022-0416

---
+++ README.md
@@ -0,0 +1,19 @@
+# Xray Shadowsocks
+
+## 概述
+
+用于在 Heroku 上部署 Xray Shadowsocks，每次部署自动选择最新的 alpine linux 和 xray core。  
+
+## 镜像
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://dashboard.heroku.com/new?template=https://github.com/Tonkercke/XSS)
+
+## 注意
+
+### 端口
+
+`端口` 为 `443` 。
+
+### 加密协议
+`AES-128-GCM`
+
---
+++ configure.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Download and install xray
+mkdir /tmp/xray
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+unzip /tmp/xray/xray.zip -d /tmp/xray
+install -m 755 /tmp/xray/xray /usr/local/bin/xray
+
+# Remove temporary directory
+rm -rf /tmp/xray
+
+# xray new configuration
+install -d /usr/local/etc/xray
+cat << EOF > /usr/local/etc/xray/config.json
+{
+  "log": {
+    "loglevel": "none"
+  },
+  "inbounds": [
+    {
+      "listen": "127.0.0.1",
+      "port": 443,
+      "protocol": "Shadowsocks",
+      "settings": {
+        "method": "AES-128-GCM",
+                "password":"$PW",
+                "network": "tcp,udp",
+                "ivCheck": true
+                }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
+EOF
+
+# Run xray
+/usr/local/bin/xray -config /usr/local/etc/xray/config.json
